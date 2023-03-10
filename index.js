1d230288c2c0739d0c6052741421beae527d985d
oliviagao9 <oliviagao9@gmail.com>
2023-03-09 21:47:36 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, string) {
+    let s = [];
+    for(let i = 0; i < names.length; i++) {
+        s.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+
+    return s;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n = n -1;
+    }
+}
